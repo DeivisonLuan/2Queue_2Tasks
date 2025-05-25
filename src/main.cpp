cf69e07c398c33a1637dacae8e4d4a2db9c5caab
DeivisonLuan <deivison.luan@live.com>
2025-05-25 16:51:40 -0300

    From PC

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,41 @@
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <freertos/queue.h>
+#include <sstream>
 #include <Arduino.h>
 
+#define Built_in_Led 2
+
+static const int serial_queue_len = 5;   // Tamanho da fila 1: Mensagens do serial
+static const int freq_queue_len = 5;     // Tamanho da fila 2: Armazena a frequencia de piscadas
+static const uint8_t blink_max = 10;   // Numero max de piscadas antes da mensagem 
+
+static QueueHandle_t queue_1; // Define fila 1: Armazena as mensagens do serial
+static QueueHandle_t queue_2; // Define fila 2: Recebe valores da fila e pisca led com frequencias usando esses valores
+
+void SerialInfo(void* parameters){
+
+}
+
+void FreqDefine(void* parameters){
+
+}
+
 void setup() {
-  pinMode(2, OUTPUT);
+  pinMode(Built_in_Led, OUTPUT);
+
+  Serial.begin(115200);
+
+  queue_1 = xQueueCreate(serial_queue_len, sizeof(50*sizeof(char)));
+  queue_2 = xQueueCreate(freq_queue_len, sizeof(int));
+
+  // Inicia tarefa A: Leitura e escrita serial
+  xTaskCreatePinnedToCore(SerialInfo, "SerialInfo", 1024, NULL, 1, NULL, 1);
+ 
+  // Inicia a tarefa B: Pisca Led com frequencia controlada
+  xTaskCreatePinnedToCore(FreqDefine, "FreqDefine", 1024, NULL, 1, NULL, 1);
 }
 
 void loop() {
-  digitalWrite(2, HIGH);
-  delay(500);
-  digitalWrite(2, LOW);
-  delay(500);
+  vTaskDelete(NULL);
 }
